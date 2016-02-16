a6700c9f0f7cef388ddf74ef9d6330154307c0e5
lhd <lhd@someone.local>
2016-02-16 09:43:52 +0800

    Initial Commit

---
+++ Test-2.16/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test-2.16
+//
+//  Created by lhd on 16/2/16.
+//  Copyright © 2016年 LHD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test-2.16/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test-2.16
+//
+//  Created by lhd on 16/2/16.
+//  Copyright © 2016年 LHD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
